14f12cdf8302c8a8588bbee72740172ed3d30cc4
“ilagarwal” <ila.agarwal@gmail.com>
2015-07-21 21:00:01 +0530

    Created Project

---
+++ TwitterFeeds/TwitterFeeds/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwitterFeeds
+//
+//  Created by Agarwal, Ila on 19/07/15.
+//  Copyright (c) 2015 Agarwal, Ila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TwitterFeeds/TwitterFeeds/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TwitterFeeds
+//
+//  Created by Agarwal, Ila on 19/07/15.
+//  Copyright (c) 2015 Agarwal, Ila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
