765ce814d16e95e8135bd48e18f7f3d52564d288
“ilagarwal” <ila.agarwal@gmail.com>
2015-07-29 01:35:55 +0530

    Added twitter timeline

---
+++ TwitterFeeds/TwitterFeeds/Utils/UIColor+colors.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+colors.h
+//  TwitterFeeds
+//
+//  Created by Agarwal, Ila on 28/07/15.
+//  Copyright (c) 2015 Agarwal, Ila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (colors)
+
++(UIColor*)twBlueColor;
+
+@end
---
+++ TwitterFeeds/TwitterFeeds/Utils/UIColor+colors.m
@@ -0,0 +1,18 @@
+//
+//  UIColor+colors.m
+//  TwitterFeeds
+//
+//  Created by Agarwal, Ila on 28/07/15.
+//  Copyright (c) 2015 Agarwal, Ila. All rights reserved.
+//
+
+#import "UIColor+colors.h"
+
+@implementation UIColor (colors)
+
+
++(UIColor*)twBlueColor{
+    return [UIColor blueColor];
+}
+
+@end
